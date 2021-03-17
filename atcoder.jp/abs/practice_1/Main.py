6cb9a062db61360bed43ecf6bc7eeefc62bae208
krimi <ayame.suzuki.job@gmail.com>
2021-03-17 23:42:13 +0900

    [AC] abs practice_1

---
+++ atcoder.jp/abs/practice_1/Main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ����������
+a = int(input())
+# ���ڡ������ڤ������������
+b, c = map(int, input().split())
+# ʸ���������
+s = input()
+# ����
+print("{} {}".format(a+b+c, s))
