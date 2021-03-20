e47378c777db601d99637ab81d8c1abf10927495
krimi <ayame.suzuki.job@gmail.com>
2021-03-20 21:33:56 +0900

    [AC] abc196 abc196_a

---
+++ atcoder.jp/abc196/abc196_a/Main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+a, b = map(int, input().split())
+c, d = map(int, input().split())
+
+#if c >= 0:
+#  m = b - c 
+##elif c <0 and d>=0:
+##  if -c >= d:
+##    m = b - c
+##  else:
+##    m = b - d
+#else:
+#  m = b - d
+ 
+m = b - c
+
+print(m)
