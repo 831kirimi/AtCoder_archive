d4b0c67c4aaac13d10b8215f16d55073c354cf27
krimi <ayame.suzuki.job@gmail.com>
2021-12-19 21:02:09 +0900

    [AC] abc232 abc232_a

---
+++ atcoder.jp/abc232/abc232_a/Main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def main():
+  n = str(input())
+  a = int(n[0])
+  b = int(n[2])
+  ans = a * b
+  print(ans)
+
+if(__name__ == '__main__'):
+  main()
